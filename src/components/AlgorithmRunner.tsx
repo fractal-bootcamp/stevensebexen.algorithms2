1b9b8b5c2e67d20d67dba9860b511e8d4e150702
Steven <ssebexen@gmail.com>
2024-06-26 15:40:00 -0400

    Removed unused.

--- src/components/AlgorithmRunner.tsx
+++ src/components/AlgorithmRunner.tsx
@@ -1,12 +1,12 @@
 import { ChangeEvent, ChangeEventHandler, MouseEvent, useState } from "react";
 
-interface AlgorithmRunnerProps<T> {
+interface AlgorithmRunnerProps {
   placeholder: string;
   onRun: () => void;
   onInputChanged: ChangeEventHandler<HTMLInputElement>;
   value: string;
 }
-export default function AlgorithmRunner<T>(props: AlgorithmRunnerProps<T>) {
+export default function AlgorithmRunner(props: AlgorithmRunnerProps) {
   return (
     <div className='flex gap-2'>
       <input className='basis-[24rem]' placeholder={props.placeholder} value={props.value} onChange={props.onInputChanged} />
